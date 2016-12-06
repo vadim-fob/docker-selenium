e76822584967cd8e48456647c73274018764ff97
vadim-fob <vadim.sinchukov@fob-solutions.com>
2016-12-06 18:36:44 +0200

    initial commit

--- Hub/generate.sh
+++ Hub/generate.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-VERSION=$1
 
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" > ./Dockerfile
 echo "# NOTE: DO *NOT* EDIT THIS FILE.  IT IS GENERATED." >> ./Dockerfile
 echo "# PLEASE UPDATE Dockerfile.txt INSTEAD OF THIS FILE" >> ./Dockerfile
 echo "# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" >> ./Dockerfile
-echo FROM selenium/base:$VERSION >> ./Dockerfile
 cat ./Dockerfile.txt >> ./Dockerfile
